d7a591bea24a6510bf5ee5719d8bd9353ce18fc6
suconpa <89624140+suconpa@users.noreply.github.com>
2023-04-25 15:48:00 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,85 +1,123 @@
-// allMemo라는 JSON 형식으로 작성된 문자열을 JS객체로 변환
+// allMemo를 JSON 형식으로 작성된 문자열을 JS객체로 변환
 let allMemo = JSON.parse(localStorage.getItem("allMemo"));
 
 // allMemo의 값이 undefined라면 [] 배열을 할당합니다.
 allMemo = allMemo ?? [];
 
+console.log(allMemo);
 // render 함수실행
 render();
 
+const saveBtn = document.querySelector("#save-btn");
+
+saveBtn.addEventListener("click", saveNote);
+
+// 저장 버튼을 눌렀을 때 실행함수 입니다.
 function saveNote() {
-  // title이라는 아이디를 가진 input의 value 즉 입력값을 title상수에 저장
-  const title = document.getElementById("title").value;
+  // input 의 입력값을 가져옵니다.
+  let $title = document.querySelector("#title").value;
 
-  // content라는 아이디를 가진 textarea의 value즉 입력값을 content상수에 저장
-  const content = document.getElementById("content").value;
+  // textarea의 value를 가져옵니다.
+  let $content = document.querySelector("#content").value;
 
-  // 배열인 allMemo에 객체를 push 하고 객체안에 값은 title의 value(text),content의 value(text), len이라는 key를가진 allMemo.length가 value로 들어가게 됩니다.
-  allMemo.push({ title, content, len: allMemo.length });
+  if (!$title) {
+    alert("내용을 입력해주세요");
+  } else {
+    console.log(132);
+    // 배열인 allMemo에 객체를 push 하고 객체안에 값은 title의 value(text),content의 value(text), len이라는 key를가진 allMemo.length가 value로 들어가게 됩니다.
+    allMemo.push({ $title, $content, len: allMemo.length });
 
-  //localStorage에 "key","value"를 저장하기위한 setItem()
-  //localStorage에 저장할 값은 title,content, allMemo.length가 됩니다.
-  //stringify를 사용하여 JSON문자열로 변환시켜줍니다.
-  localStorage.setItem("allMemo", JSON.stringify(allMemo));
+    //localStorage에 "key","value"를 저장하기위한 setItem()
+    //localStorage에 저장할 값은 title,content, allMemo.length가 됩니다.
+    //stringify를 사용하여 JSON문자열로 변환시켜줍니다.
+    localStorage.setItem("allMemo", JSON.stringify(allMemo));
 
-  //render함수 실행
-  render();
+    //render함수 실행
+    render();
+    document.querySelector("#title").value = "";
+    document.querySelector("#content").value = "";
+  }
 }
 
 function render() {
-  // display id를 가진 <section>
-  const display = document.getElementById("display");
-  //  <section>안에 있는 text를 문자열로 지정
-  display.innerHTML = "";
+  // ul의 상위부모
+  const $display = document.querySelector("#display");
 
-  // 배열의 length만큼 순회하는 for in문 사용
+  // 메모리스트를 품고있는 <ul>
+  const $memolist = document.querySelector(".memo-list");
+
+  // 함수가 실행 될 때마다 값이 초기화
+  $memolist.innerHTML = "";
+
   for (const item of allMemo) {
-    // html에 <h2> 태그를 생성할 상수
-    const saveTitle = document.createElement("h2");
+    // ul안에 들어갈 자식을 생성
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    const div2 = document.createElement("div");
+    const article = document.createElement("article");
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+    const viewBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
 
-    // html에 <p> 태그를 생성할 상수
-    const saveContent = document.createElement("p");
+    // ul안에 들어갈 자식들의 className 추가
+    li.classList.add("list-wrap");
+    div.classList.add("list-num");
+    div2.classList.add("button-wrap");
+    viewBtn.classList.add("view");
+    deleteBtn.classList.add("delete");
 
-    // html에 <p> 태그를 생성할 상수
-    const saveId = document.createElement("p");
+    // 각각의 자식을생성
+    $memolist.append(li);
+    li.appendChild(div);
+    li.appendChild(article);
+    li.appendChild(div2);
+    article.appendChild(h3);
+    article.appendChild(p);
+    div2.appendChild(viewBtn);
+    div2.appendChild(deleteBtn);
 
-    //   html에 <button> 태그를 생성할 상수
-    const deleteMemoBtn = document.createElement("button");
+    deleteBtn.setAttribute("id", item.len); // 왜 index가 0부터 알아서 들어갈까...?
 
-    //만든 h2태그의 text는 allMemo에 0번 index ~for
-    saveTitle.textContent = item.title;
+    // 제목,내용에 allMemo에 들어있는 값 대입
+    h3.textContent = item.$title;
+    p.textContent = item.$content;
+    div.textContent = `No.${item.len + 1}`;
+    viewBtn.innerHTML = `more`;
+    deleteBtn.innerHTML = `<i class="fa-regular fa-trash-can"></i>`;
 
-    //만든 첫번째 <p> 태그의 text는 allMemo에 0번째 index  ~for
-    saveContent.textContent = item.content;
+    deleteBtn.addEventListener("click", remove);
 
-    //만든 두번째 <p> 태그에 text는 allMemo에 length +1
-    saveId.textContent = item.len + 1;
+    // 더보기 버튼 이벤트
+    viewBtn.addEventListener("click", () => {
+      const modal = document.querySelector(".modal");
+      const modalCloseBtn = modal.querySelector("#modal-close-button");
+      const modalH3 = modal.querySelector("h3");
+      const modalP = modal.querySelector("p");
+      // modal 안에 들어갈 제목
 
-    //만든 <button> 태그의 text는 "삭제"
-    deleteMemoBtn.textContent = "삭제";
+      // 더보기 버튼을 누르면 모달 class추가
+      const bgOpacity = document.querySelector(".background-opacity");
 
-    //<button> 태그의 id를 allMemo안에 있는 key중 len으로 지정
-    deleteMemoBtn.setAttribute("id", item.len);
+      bgOpacity.classList.add("bg-opacity");
+      // 더보기 버튼을 누르면 모달 class추가
+      const modalHidden = document.querySelector(".modal");
+      modalHidden.classList.add("modal-hidden");
 
-    //<button>태그의 onclick이벤트가 발행사면 remove함수 실행
-    deleteMemoBtn.setAttribute("onclick", "remove()");
+      modalH3.textContent = item.$title;
+      modalP.textContent = item.$content;
 
-    // <section id=display> 의 자식으로 <p> 를 위치함
-    display.appendChild(saveId);
-
-    // <section id=display> 의 자식으로 <h2> 를 위치함
-    display.appendChild(saveTitle);
-
-    // <section id=display> 의 자식으로 <p> 를 위치함
-    display.appendChild(saveContent);
-
-    // <section id=display> 의 자식으로 <button> 를 위치함
-    display.appendChild(deleteMemoBtn);
+      modalCloseBtn.addEventListener("click", () => {
+        bgOpacity.classList.remove("bg-opacity");
+        modalHidden.classList.remove("modal-hidden");
+      });
+    });
   }
 }
 
 // 로컬스토리지에 저장되어있는 값 제거
 function remove() {
+  console.log(event.srcElement.id);
   // allMemo에 값중 event.secElement.id 와 같은 값을
   // idx상수에 return
   const idx = allMemo.find((item) => item.len == event.srcElement.id);
